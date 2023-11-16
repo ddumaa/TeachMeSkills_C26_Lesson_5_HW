8797ec932f1f12d02431a678130f43d080c8e7e2
ddumaa <dimaduma62@gmail.com>
2023-11-16 15:54:14 +0300

    update 2 fixing the program

--- src/hommework/HWA2.java
+++ src/hommework/HWA2.java
@@ -1,19 +1,26 @@
 package hommework;
 
+import java.util.Arrays;
+
 /**
  * Умножение двух матриц
- * 	Создайте два массива целых чисел размером 3х3 (две матрицы).
+ * 	Создайте два массива целых чисел (две матрицы).
  * 	Напишите программу для умножения двух матриц.
  */
 
 public class HWA2 {
     public static void main(String[] args) {
-        int [][] arrays1 = {{1,1,2},{0,1,0},{2,4,0}};
-        int [][] arrays2 = {{1,2,3},{1,1,1},{2,1,7}};
-        for (int i = 0; i < arrays1.length; i++) {
-            for (int j = 0; j < arrays1[i].length; j++) {
-                System.out.println(arrays1[i][j] * arrays2[i][j]);
+        int [][] arrays1 = {{1,0,0,0},{0,1,0,0},{0,0,0,0}};
+        int [][] arrays2 = {{1,2,3},{1,1,1},{0,0,0},{2,1,0}};
+        int [][] arrays3 = new int[arrays1.length][arrays2[0].length];
+
+        for (int o = 0; o < arrays3.length; o++) {
+            for (int i = 0; i < arrays3.length; i++) {
+                for (int j = 0; j < arrays1[0].length; j++) {
+                arrays3[o][i] += arrays1[o][j] * arrays2[j][i];
+                }
             }
         }
+        System.out.println(Arrays.deepToString(arrays3));
     }
 }
