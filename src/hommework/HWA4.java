5f8d86fd062a32ae6bf28942735d18ce8aec6311
ddumaa <dimaduma62@gmail.com>
2023-11-18 01:04:46 +0300

    update 2.1

--- src/hommework/HWA4.java
+++ src/hommework/HWA4.java
@@ -9,23 +9,19 @@ import java.util.Arrays;
 public class HWA4 {
     public static void main(String[] args) {
         int[][] array = new int[4][4];
-        int numbers = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = (int)(Math.random() * 10);
-                if(array[i][j] == array[numbers][numbers]){
-                    System.out.println(array[numbers][numbers]);
-                    numbers++;
+                if(i == j){
+                    System.out.println(array[i][j]);
                 }
             }
         }
         System.out.println(" ");
-        int numbers2 = array.length-1;
         for (int i = array.length-1; i>=0; i--) {
             for (int j = array[i].length-1; j >=0; j--) {
-                if(array[i][j] == array[numbers2][numbers2]) {
-                    System.out.println(array[numbers2][numbers2]);
-                    numbers2--;
+                if(i == j) {
+                    System.out.println(array[i][j]);
                 }
             }
         }
